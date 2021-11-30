460635ed4dd9b633fcbfccd360ab49014d363fa9
BirdZhang <0312birdzhang@gmail.com>
2021-11-30 09:29:38 +0000

    add china libinit

--- libinit/init_xiaomi_nabu.cpp
+++ libinit/init_xiaomi_nabu.cpp
@@ -12,6 +12,7 @@
 #define DESCRIPTION "nabu_global-user 11 RKQ1.200826.002 V12.5.3.0.RJUMIXM release-keys"
 #define FINGERPRINT "Xiaomi/nabu_global/nabu:11/RKQ1.200826.002/V12.5.3.0.RJUMIXM:user/release-keys"
 
+
 static const variant_info_t nabu_global_info = {
     .hwc_value = "GLOBAL",
     .sku_value = "",
@@ -23,13 +24,26 @@ static const variant_info_t nabu_global_info = {
     .build_description = DESCRIPTION,
     .build_fingerprint = FINGERPRINT,
 
-    .nfc = true,
+    .nfc = false,
 };
 
 
+static const variant_info_t nabu_china_info = {
+    .hwc_value = "CHINA",
+    .sku_value = "",
 
+    .brand = "Xiaomi",
+    .device = "nabu",
+    .marketname = "Xiaomi Pad 5",
+    .model = "21051182C",
+    .build_description = DESCRIPTION,
+    .build_fingerprint = FINGERPRINT,
+
+    .nfc = false,
+};
 static const std::vector<variant_info_t> variants = {
     nabu_global_info,
+    nabu_china_info,
 };
 
 void vendor_load_properties() {
