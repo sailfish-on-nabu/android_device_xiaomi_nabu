e3c4d2fd771bcfac1b8363e9db03eb68a1bf2da4
BirdZhang <0312birdzhang@gmail.com>
2021-12-04 19:28:38 +0800

    fixup defconfig path

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,7 +22,7 @@ TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_nabu
 TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_nabu
 
 # Kernel
-TARGET_KERNEL_CONFIG := vendor/nabu_defconfig
+TARGET_KERNEL_CONFIG := nabu_user_defconfig
 
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
